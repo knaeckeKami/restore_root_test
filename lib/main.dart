fb5fb92428aad7b425806c8293f9fd949f228ecf
Martin Kamleithner <martin.kamleithner@gmail.com>
2023-03-10 23:11:11 +0100

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:restore_root_test/second_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      restorationScopeId: 'app',
+      home:  MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+
+      body: Center(
+
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).restorablePush(SecondPage.secondPageRouteBuilderWithPragma);
+              },
+              child: const Text('Second Page static pragma'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).restorablePush(SecondPage.secondPageRouteBuilderNoPragma);
+              },
+              child: const Text('Second Page static no pragma'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).restorablePush(secondPageRouteBuilderWithPragma);
+              },
+              child: const Text('Second Page top level with pragma'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).restorablePush(secondPageRouteBuilderNoPragma);
+              },
+              child: const Text('Second Page top level no pragma'),
+            ),
+          ],
+        ),
+    ));
+  }
+}
+
---
+++ lib/second_page.dart
@@ -0,0 +1,39 @@
+
+import 'package:flutter/material.dart';
+
+
+Route secondPageRouteBuilderNoPragma(BuildContext context, Object? args) {
+  return MaterialPageRoute<void>(
+    builder: (context) => const SecondPage(),
+  );
+}
+
+@pragma('vm:entry-point')
+Route secondPageRouteBuilderWithPragma(BuildContext context, Object? args) {
+  return MaterialPageRoute<void>(
+    builder: (context) => const SecondPage(),
+  );
+}
+
+class SecondPage extends StatelessWidget {
+  const SecondPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+
+
+  static Route secondPageRouteBuilderNoPragma(BuildContext context, Object? args) {
+    return MaterialPageRoute<void>(
+      builder: (context) => const SecondPage(),
+    );
+  }
+
+  @pragma('vm:entry-point')
+  static Route secondPageRouteBuilderWithPragma(BuildContext context, Object? args) {
+    return MaterialPageRoute<void>(
+      builder: (context) => const SecondPage(),
+    );
+  }
+}
